ea48c3aa635842f995af082df9d7aed5178bfd27
Daniel Garcia Sanchez <dontic@zoho.com>
2023-11-05 20:10:32 +0000

    Update static url

--- rentake/settings.py
+++ rentake/settings.py
@@ -109,7 +109,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "django_static"
+STATIC_URL = "/django_static/"
 
 # Set media root
 MEDIA_ROOT = BASE_DIR / "media"
