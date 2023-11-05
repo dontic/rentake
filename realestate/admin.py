7dd7d9926fbdae93f6d6b05aa2669fbab3e33a0e
Daniel Garcia Sanchez <dontic@zoho.com>
2023-11-05 19:19:45 +0000

    Update models and admin

--- realestate/admin.py
+++ realestate/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "phone")
+    search_fields = ("first_name", "last_name", "email", "phone")
+
+
+@admin.register(RentalAsset)
+class RentalAssetAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "catastral_reference",
+        "is_rented",
+    )
+    search_fields = (
+        "title",
+        "catastral_reference",
+        "parking_number",
+        "number_of_bedrooms",
+        "number_of_bathrooms",
+        "address_line_1",
+        "address_line_2",
+        "postcode",
+        "city",
+        "province",
+        "country",
+        "is_rented",
+    )
+    list_filter = ("is_rented",)
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = (
+        "tenant",
+        "start_date",
+        "end_date",
+        "price",
+        "deposit",
+    )
+    search_fields = (
+        "tenant",
+        "start_date",
+        "end_date",
+        "price",
+        "deposit",
+    )
+    list_filter = ("start_date", "end_date")
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = (
+        "rental_asset",
+        "price",
+        "start_date",
+        "end_date",
+        "is_active",
+    )
+    search_fields = (
+        "rental_asset",
+        "price",
+        "start_date",
+        "end_date",
+        "is_active",
+    )
+    list_filter = ("start_date", "end_date", "is_active")
--- realestate/models.py
+++ realestate/models.py
@@ -6,17 +6,28 @@ class Tenant(models.Model):
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     phone = models.CharField(max_length=200)
+    NIF = models.CharField(max_length=200)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
-class Property(models.Model):
+class RentalAsset(models.Model):
+    HOUSE_TYPE = "house"
+    PARKING_TYPE = "parking"
+
+    ASSET_TYPES = [
+        (HOUSE_TYPE, "House"),
+        (PARKING_TYPE, "Parking"),
+    ]
+
+    type = models.CharField(max_length=10, choices=ASSET_TYPES, default=HOUSE_TYPE)
     title = models.CharField(max_length=200)
     catastral_reference = models.CharField(max_length=200)
-    storage_room_number = models.CharField(max_length=200, blank=True, null=True)
-    number_of_bedrooms = models.IntegerField()
-    number_of_bathrooms = models.IntegerField()
+    parking_number = models.CharField(max_length=200, blank=True, null=True)
+    number_of_bedrooms = models.IntegerField(blank=True, null=True)
+    number_of_bathrooms = models.IntegerField(blank=True, null=True)
+    storage_room = models.IntegerField(blank=True, null=True)
     address_line_1 = models.CharField(max_length=200)
     address_line_2 = models.CharField(max_length=200, blank=True, null=True)
     postcode = models.CharField(max_length=200)
@@ -26,40 +37,29 @@ class Property(models.Model):
     is_rented = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.catastral_reference}"
 
 
-class Parking(models.Model):
-    number = models.CharField(max_length=200)
-    floor = models.IntegerField()
-    associated_property = models.ForeignKey(
-        Property, on_delete=models.SET_NULL, blank=True, null=True
-    )
-    address_line_1 = models.CharField(max_length=200)
-    address_line_2 = models.CharField(max_length=200, blank=True, null=True)
-    postcode = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    province = models.CharField(max_length=200)
-    postcode = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
-    is_rented = models.BooleanField(default=False)
+class Price(models.Model):
+    rental_asset = models.ForeignKey(RentalAsset, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.number}"
+        return f"{self.rental_asset} - {self.price}"
 
 
 class Contract(models.Model):
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
-    property = models.ForeignKey(
-        Property, on_delete=models.SET_NULL, blank=True, null=True
-    )
-    parkings = models.ManyToManyField(Parking, blank=True)
+    rental_assets = models.ManyToManyField(RentalAsset)
     start_date = models.DateField()
     end_date = models.DateField(blank=True, null=True)
-    rent = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     deposit = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
     file = models.FileField(upload_to="contracts", blank=True, null=True)
 
     def __str__(self):
-        return f"{self.tenant} - {self.property}"
+        return f"{self.tenant} - {self.rental_assets}"
