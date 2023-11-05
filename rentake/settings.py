2052e7a1c19912b915a8144c6a3e5c21486d84f2
Daniel Garcia Sanchez <dontic@zoho.com>
2023-11-05 20:16:21 +0000

    Set up allowed hosts

--- rentake/settings.py
+++ rentake/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "django-insecure-j0saf5_)r%bkyn_ma&^obdcrayel!lfz0a4je$$z$700sich6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
