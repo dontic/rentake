044ba80bcf78599b9c16d781e4bec04dd43800e6
Daniel Garcia Sanchez <dontic@zoho.com>
2023-11-06 08:17:01 +0000

    Update admin.py
    Create list fields and filter for manytomany fields

--- realestate/admin.py
+++ realestate/admin.py
@@ -33,23 +33,55 @@ class RentalAssetAdmin(admin.ModelAdmin):
     list_filter = ("is_rented",)
 
 
+class RentalAssetTitleFilter(admin.SimpleListFilter):
+    title = "Rental Asset Title"
+    parameter_name = "title"
+
+    def lookups(self, request, model_admin):
+        rental_assets = set()
+        for obj in model_admin.model.objects.all():
+            for rental_asset in obj.rental_assets.all():
+                rental_assets.add(rental_asset)
+        return [(rental_asset.id, rental_asset.title) for rental_asset in rental_assets]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(title=self.value())
+        else:
+            return queryset
+
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
     list_display = (
-        "tenant",
+        "list_tenants",
+        "list_rental_assets",
         "start_date",
         "end_date",
         "price",
         "deposit",
     )
     search_fields = (
-        "tenant",
         "start_date",
         "end_date",
         "price",
-        "deposit",
-    )
-    list_filter = ("start_date", "end_date")
+        "deposit", 
+    ) 
+    list_filter = (RentalAssetTitleFilter, "start_date", "end_date",)
+
+    def list_tenants(self, obj):
+        return ", ".join([tenant.first_name for tenant in obj.tenants.all()])
+    list_tenants.short_description = "Tenants"
+
+    def list_rental_assets(self, obj):
+        return ", ".join([rental_asset.title for rental_asset in obj.rental_assets.all()])
+    list_rental_assets.short_description = "Rental Assets"
+
+    def catrastral_reference(self, obj):
+        return obj.rental_assets.catastral_reference
+    catrastral_reference.short_description = "Catastral Reference"
+
+
+
 
 
 @admin.register(Price)
