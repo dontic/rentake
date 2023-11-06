c86d77f0fde27eb6f38a42ff8c2bf31445eb7e62
Daniel Garcia Sanchez <dontic@zoho.com>
2023-11-06 08:15:18 +0000

    Update settings.py
    Fix default vallue for CSRF_TRUSTED_ORIGINS

--- rentake/settings.py
+++ rentake/settings.py
@@ -24,7 +24,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8000,http://127.0.0.1:8000").split(",")
 
 # Application definition
 
